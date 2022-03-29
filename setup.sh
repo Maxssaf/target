dac7604cb36f6501a24262b80cc7fd2686ea1434
Maxssaf <101875980+Maxssaf@users.noreply.github.com>
2022-03-29 16:07:54 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,20 +9,20 @@ pkill -f ./start.py
 sudo apt update  -y
 sudo apt install git python3 python3-pip  -y
 # for some virtual cloud systems based on debian (like GC)
-sudo apt install gcc libc-dev libffi-dev libssl-dev python3-dev rustc  -y 
+sudo apt install git gcc libc-dev libffi-dev libssl-dev python3-dev rustc  -y 
 sudo pip install --upgrade pip 
 
 
 # Install git, python3
 #sudo apt install git gcc libc-dev libffi-dev libssl-dev python3-dev rustc -y
 #sudo apt install git -y
-sudo apt upgrade git -y
+#sudo apt upgrade git -y
 sudo apt install wget -y
 sudo apt upgrade wget -y
 #sudo apt install python3 -y
-sudo apt upgrade python3 -y
+#sudo apt upgrade python3 -y
 #sudo apt install python3-pip -y
-sudo apt upgrade python3-pip -y
+#sudo apt upgrade python3-pip -y
 sudo apt install screen -y
 sudo apt upgrade screen -y
 sudo apt install curl -y
